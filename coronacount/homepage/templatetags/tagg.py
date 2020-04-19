0a090a85b51ce4f625795c3c095d1b23ab97c991
i-gitit <51433764+i-gitit@users.noreply.github.com>
2020-04-19 12:31:32 +0530

    'homepagelayout'

--- coronacount/homepage/templatetags/tagg.py
+++ coronacount/homepage/templatetags/tagg.py
@@ -5,3 +5,8 @@ register = template.Library()
 @register.filter(name='comma')
 def comma(value):
     return int(value.replace(',', ''))
+
+
+@register.filter(name='plus')
+def plus(value):
+    return value.replace('+', '')
